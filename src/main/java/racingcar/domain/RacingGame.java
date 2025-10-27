bba11b5e23c387af4a7df0856ea1d65cc0318211
suho <sshsh1004@naver.com>
2025-10-27 15:11:13 +0900

    feat: 자동차 이동 로직 및 랜덤 값 기반 전진 기능 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,25 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class RacingGame {
+    private final List<Car> cars;
+
+    public RacingGame(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void playRound() {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            car.move(randomNumber);
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
 }
